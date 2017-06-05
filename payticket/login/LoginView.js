0d2b54cb68f917ab3337888b1135fcc19b3ef4d8
huangbin <575395982@qq.com>
2017-06-06 02:02:36 +0800

    ADD 添加网络工具类

--- payticket/login/LoginView.js
+++ payticket/login/LoginView.js
@@ -4,34 +4,59 @@
 var React  = require("react");
 
 var ReactNative = require("react-native");
+var NetUtils = require("../utils/NetUtils");
 
 var {
     View,
     Text,
     TouchableOpacity,
-    AlertIOS
+    AlertIOS,
+    Image,
+    StyleSheet
 }  = ReactNative;
 
+
+var StyleObj = StyleSheet.create({
+    rooView: {
+        flex:1,
+        alignItems:'center'
+    },
+
+    logoImg: {
+        marginTop:'30%'
+    },
+
+    loginBtn: {
+        width: '50%',
+        height: '5%',
+        backgroundColor: '#01aaff',
+        borderRadius: 2,
+        alignItems: 'center',
+    },
+
+    loginTv: {
+        fontSize:23,
+        color:'#fff',
+        alignItems: 'center',
+        justifyContent:'center'
+    }
+});
 
 var LoginView = React.createClass({
     render: function () {
         return (
-            <View>
-                <Text>我是登录界面,我等</Text>
+            <View style={StyleObj.rooView}>
+
+                <Image
+                    style={StyleObj.logoImg}
+                    source={require("../../img/hello_log.png")}
+                />
 
                 <TouchableOpacity
-                    style={
-                        {
-                            width:'50%',
-                            backgroundColor:'#f00',
-                            marginTop:'10%',
-                            marginLeft:'10%',
-                            borderRadius:10,
-                        }
-                    }
+                    style={StyleObj.loginBtn}
                     onPress={this._onBtnClick.bind(this)}
                 >
-                    <Text>按钮</Text>
+                    <Text style={StyleObj.loginTv}>登录</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -40,6 +65,8 @@ var LoginView = React.createClass({
     _onBtnClick: function () {
         // AlertIOS.prompt("答案", "默认答案",null);
         this.props.navigation.navigate('Tab', { name: 'two' });
+
+        NetUtils.get(NetUtils.DouB_Api, null);
     }
 });
 
---
+++ payticket/utils/NetUtils.js
@@ -0,0 +1,134 @@
+
+
+class NetUtils {
+
+    static DouB_Api = 'https://api.douban.com/v2/movie/top250?start=0&count=20';
+    /**
+     * post请求
+     * url : 请求地址
+     * data : 参数(Json对象)
+     * callback : 回调函数
+     * */
+    static postJson(url, data, callback) {
+        var fetchOption = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        };
+
+        fetch(url, fetchOption)
+            .then((response) => response.text())
+            .then((responseText) => {
+                callback(JSON.parse(responseText))
+            })
+            .done();
+    }
+
+    /**
+     * get请求
+     * url : 请求地址
+     * callback : 回调函数
+     */
+    static get(url, callback) {
+        var fetchOptions = {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        };
+        fetch(url, fetchOptions)
+            .then((response) => response.text())
+            .then((responseText) => {
+                callback(JSON.parse(responseText));
+            }).done();
+    }
+
+
+
+
+    /*
+     *  get请求
+     *  url:请求地址
+     *  params:参数
+     *  callback:回调函数
+     * */
+    static get(url,params,callback){
+        if (params) {
+            let paramsArray = [];
+            //拼接参数
+            Object.keys(params).forEach(key => paramsArray.push(key + '=' + params[key]))
+            if (url.search(/\?/) === -1) {
+                url += '?' + paramsArray.join('&')
+            } else {
+                url += '&' + paramsArray.join('&')
+            }
+        }
+        //fetch请求
+        fetch(url,{
+            method: 'GET',
+        })
+            .then((response) => {
+                callback(response)
+            })
+            .catch((error) => {
+                alert(error)
+            });
+    }
+
+
+    /*  第一种：'Content-Type': 'application/json'
+     *  post请求
+     *  url:请求地址
+     *  params:参数,这里的参数格式是：{param1: 'value1',param2: 'value2'}
+     *  callback:回调函数
+     * */
+    static postJSON(url,params,callback){
+        //fetch请求
+        fetch(url,{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify(params)
+        })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                callback(responseJSON)
+            })
+            .catch((error) => {
+                console.log("error = " + error)
+            });
+    }
+
+    /*  第二种： form表单形式
+     *  post请求
+     *  url:请求地址
+     *  params:参数,这里的参数要用这种格式：'key1=value1&key2=value2'
+     *  callback:回调函数
+     * */
+    static postForm(url,params,callback){
+        //fetch请求
+        fetch(url,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: params
+        })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                callback(responseJSON)
+            })
+            .catch((error) => {
+                console.log("error = " + error)
+            });
+    }
+
+}
+
+export default NetUtils;
